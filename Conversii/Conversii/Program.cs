b6f2ecf7779ff305f4b804cb3a8c7d65c71cc135
Erik Lazin <154979602+LazinErik@users.noreply.github.com>
2024-01-19 23:54:13 +0200

    Add files via upload

---
+++ Conversii/Conversii/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Conversii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nrBinar = "";
+            int nrZecimal = 0;
+            int putere = 0;
+            Console.Write("Numarul in binar este: ");
+            nrBinar = Console.ReadLine();
+            while (nrBinar.Length > 0)
+            {
+                nrZecimal += (int.Parse(nrBinar[nrBinar.Length - 1].ToString())) * (int)Math.Pow(2, putere);
+                nrBinar = nrBinar.Substring(0, nrBinar.Length - 1);
+                putere++;
+            }
+            Console.WriteLine("Numarul in zecimal este: " + nrZecimal);
+        }
+    }
+}
